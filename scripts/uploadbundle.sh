c04a237839e59efd2832497de06b423d142173c8
Kevin Peraza <kevin@basistheory.com>
2025-01-31 17:25:01 -0600

    feat: update cdn script and package.json (#2)

--- scripts/uploadbundle.sh
+++ scripts/uploadbundle.sh
@@ -38,11 +38,11 @@ if ! [[ -z "${BLOB}" ]]; then
   BLOB_DIR=blob
   BLOB_PATH=$BLOB_DIR/$(git rev-parse --short HEAD).js
 
-  echo "Cloudflare R2 Uploading SDK bundle to $BUNDLE_HOST/$BLOB_PATH"
+  echo "Cloudflare R2 Uploading SDK bundle to $BUNDLE_HOST/web-threeds/$BLOB_PATH"
   rclone --config .rclone.conf \
     --s3-access-key-id ${R2_ACCESS_KEY} \
     --s3-secret-access-key ${R2_SECRET_KEY} \
-    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/${BLOB_PATH}
+    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/web-threeds/${BLOB_PATH}
 fi
 
 if ! [[ -z "${CLOUDFLARE}" ]]; then
@@ -50,19 +50,19 @@ if ! [[ -z "${CLOUDFLARE}" ]]; then
   rclone --config .rclone.conf \
     --s3-access-key-id ${R2_ACCESS_KEY} \
     --s3-secret-access-key ${R2_SECRET_KEY} \
-    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/${LATEST_VERSION_PATH}
+    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/web-threeds/${LATEST_VERSION_PATH}
 
   echo "Cloudflare R2 Uploading SDK bundle to $BUNDLE_HOST/$MAJOR_VERSION_PATH"
   rclone --config .rclone.conf \
     --s3-access-key-id ${R2_ACCESS_KEY} \
     --s3-secret-access-key ${R2_SECRET_KEY} \
-    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/${MAJOR_VERSION_PATH}
+    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/web-threeds/${MAJOR_VERSION_PATH}
 
   echo "Cloudflare R2 Uploading SDK bundle to $BUNDLE_HOST/$MINOR_VERSION_PATH"
   rclone --config .rclone.conf \
     --s3-access-key-id ${R2_ACCESS_KEY} \
     --s3-secret-access-key ${R2_SECRET_KEY} \
-    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/${MINOR_VERSION_PATH}
+    copyto --verbose ${BUNDLE_PATH} r2:${ENVIRONMENT}-3ds/web-threeds/${MINOR_VERSION_PATH}
 fi
 
 result=$?
